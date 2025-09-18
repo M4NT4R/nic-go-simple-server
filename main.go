7ab4a1680069306f0d8fb4db2c01d15a84c63b78
ManTa <144096806+M4NT4R@users.noreply.github.com>
2025-09-18 20:35:17 +0500

    Update main.go

--- main.go
+++ main.go
@@ -7,24 +7,19 @@ import (
 )
 
 func main() {
-	// эндпоинт /ping
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		response := map[string]string{"message": "pong"}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
 	})
 
-	// эндпоинт /sum?a=2&b=3
 	http.HandleFunc("/sum", func(w http.ResponseWriter, r *http.Request) {
-		// достаём query параметры
 		aStr := r.URL.Query().Get("a")
 		bStr := r.URL.Query().Get("b")
 
-		// конвертация строки в число
 		a, err1 := strconv.Atoi(aStr)
 		b, err2 := strconv.Atoi(bStr)
 
-		// проверка на ошибки
 		if err1 != nil || err2 != nil {
 			http.Error(w, "invalid query params", http.StatusBadRequest)
 			return
@@ -35,6 +30,6 @@ func main() {
 		json.NewEncoder(w).Encode(result)
 	})
 
-	// запуск сервера на порту 8080
 	http.ListenAndServe(":8080", nil)
 }
+
